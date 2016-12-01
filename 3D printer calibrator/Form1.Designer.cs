23f45f2822dc667da11ca9681d3331039943e330
Antti Jaaniste <jaantti@gmail.com>
2016-12-01 11:41:32 +0200

    Added source

---
+++ 3D printer calibrator/Form1.Designer.cs
@@ -0,0 +1,213 @@
+﻿namespace _3D_printer_calibrator {
+    partial class Form1 {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.label1 = new System.Windows.Forms.Label();
+            this.oldX = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.oldY = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.Y2 = new System.Windows.Forms.TextBox();
+            this.Y1 = new System.Windows.Forms.TextBox();
+            this.X1 = new System.Windows.Forms.TextBox();
+            this.X2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.calculate = new System.Windows.Forms.Button();
+            this.newX = new System.Windows.Forms.TextBox();
+            this.NewY = new System.Windows.Forms.TextBox();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(36, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Old X:";
+            // 
+            // oldX
+            // 
+            this.oldX.Location = new System.Drawing.Point(54, 10);
+            this.oldX.Name = "oldX";
+            this.oldX.Size = new System.Drawing.Size(56, 20);
+            this.oldX.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(116, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(36, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Old Y:";
+            // 
+            // oldY
+            // 
+            this.oldY.Location = new System.Drawing.Point(158, 10);
+            this.oldY.Name = "oldY";
+            this.oldY.Size = new System.Drawing.Size(57, 20);
+            this.oldY.TabIndex = 2;
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.X2);
+            this.panel1.Controls.Add(this.X1);
+            this.panel1.Controls.Add(this.Y1);
+            this.panel1.Controls.Add(this.Y2);
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Location = new System.Drawing.Point(15, 51);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(200, 200);
+            this.panel1.TabIndex = 3;
+            // 
+            // panel2
+            // 
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel2.Location = new System.Drawing.Point(50, 51);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(100, 100);
+            this.panel2.TabIndex = 2;
+            // 
+            // Y2
+            // 
+            this.Y2.Location = new System.Drawing.Point(50, 25);
+            this.Y2.Name = "Y2";
+            this.Y2.Size = new System.Drawing.Size(100, 20);
+            this.Y2.TabIndex = 6;
+            // 
+            // Y1
+            // 
+            this.Y1.Location = new System.Drawing.Point(50, 157);
+            this.Y1.Name = "Y1";
+            this.Y1.Size = new System.Drawing.Size(100, 20);
+            this.Y1.TabIndex = 5;
+            // 
+            // X1
+            // 
+            this.X1.Location = new System.Drawing.Point(3, 89);
+            this.X1.Name = "X1";
+            this.X1.Size = new System.Drawing.Size(41, 20);
+            this.X1.TabIndex = 3;
+            // 
+            // X2
+            // 
+            this.X2.Location = new System.Drawing.Point(152, 89);
+            this.X2.Name = "X2";
+            this.X2.Size = new System.Drawing.Size(43, 20);
+            this.X2.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(221, 164);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(42, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "New X:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(221, 209);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(42, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "New Y:";
+            // 
+            // calculate
+            // 
+            this.calculate.Location = new System.Drawing.Point(224, 51);
+            this.calculate.Name = "calculate";
+            this.calculate.Size = new System.Drawing.Size(137, 110);
+            this.calculate.TabIndex = 7;
+            this.calculate.Text = "Calculate";
+            this.calculate.UseVisualStyleBackColor = true;
+            this.calculate.Click += new System.EventHandler(this.calculate_Click);
+            // 
+            // newX
+            // 
+            this.newX.Location = new System.Drawing.Point(222, 181);
+            this.newX.Name = "newX";
+            this.newX.Size = new System.Drawing.Size(100, 20);
+            this.newX.TabIndex = 8;
+            // 
+            // NewY
+            // 
+            this.NewY.Location = new System.Drawing.Point(221, 225);
+            this.NewY.Name = "NewY";
+            this.NewY.Size = new System.Drawing.Size(100, 20);
+            this.NewY.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(373, 263);
+            this.Controls.Add(this.NewY);
+            this.Controls.Add(this.newX);
+            this.Controls.Add(this.calculate);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.oldY);
+            this.Controls.Add(this.oldX);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Printer calibration tool";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox oldX;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox oldY;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.TextBox X2;
+        private System.Windows.Forms.TextBox X1;
+        private System.Windows.Forms.TextBox Y1;
+        private System.Windows.Forms.TextBox Y2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button calculate;
+        private System.Windows.Forms.TextBox newX;
+        private System.Windows.Forms.TextBox NewY;
+    }
+}
+
---
+++ 3D printer calibrator/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _3D_printer_calibrator {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e) {
+
+        }
+
+        private void calculate_Click(object sender, EventArgs e)
+        {
+            string errors = "";
+            TextBox[] values = {oldX, oldY, X1, X2, Y1, Y2};
+            float[] numbers = new float[6];
+
+            int i = 0;
+
+            foreach (var box in values)
+            {
+                errors += parseTextbox(box, out numbers[i]);
+                i++;
+            }
+
+            if (errors.Length > 0)
+            {
+                MessageBox.Show(errors, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            float deltaX = numbers[2] - numbers[3];
+            float deltaY = numbers[4] - numbers[5];
+
+            float newXValue = numbers[0] + deltaX/2.0f;
+            float newYValue = numbers[1] + deltaY/2.0f;
+
+            newX.Text = "" + newXValue;
+            NewY.Text = "" + newYValue;
+        }
+
+        private string parseTextbox(TextBox box, out float value)
+        {
+            bool success = float.TryParse(box.Text.Replace(".", ","), out value);
+            if (success)
+            {
+                return "";
+            }
+            return box.Name + " parse error\n";
+        }
+    }
+}
